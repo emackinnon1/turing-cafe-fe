df9fe66452b6f3d65be24747adf9194ced759803
emackinnon1 <emackinnon100390@gmail.com>
2020-05-21 11:02:28 -0600

    Form smoke screen test is now passing

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -6,6 +6,15 @@ import "@testing-library/jest-dom/";
 describe("Form", () => {
 	it("should render the correct information", () => {
 		const { getByPlaceholderText, debug } = render(<Form />);
-		debug();
+
+		const nameInput = getByPlaceholderText("Name");
+		const dateInput = getByPlaceholderText("Date");
+		const timeInput = getByPlaceholderText("Time");
+		const numberInput = getByPlaceholderText("Number of guests");
+
+		expect(nameInput).toBeInTheDocument();
+		expect(dateInput).toBeInTheDocument();
+		expect(timeInput).toBeInTheDocument();
+		expect(numberInput).toBeInTheDocument();
 	});
 });
