fa7527256820495b5eeb30839b441cb2cfb98ed2
emackinnon1 <emackinnon100390@gmail.com>
2020-05-21 10:33:53 -0600

    User can add  reservation with the newly built Form component

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import { getReservations } from "../apiCalls";
 import Reservations from "../Reservations/Reservations";
+import Form from "../Form/Form";
 
 class App extends Component {
 	state = {
@@ -18,11 +19,20 @@ class App extends Component {
 		});
 	};
 
+	addReservation = (reservation) => {
+		this.setState({ reservations: [...this.state.reservations, reservation] });
+	};
+
 	render() {
 		return (
 			<div className="App">
 				<h1 className="app-title">Turing Cafe Reservations</h1>
-				<div className="resy-form"></div>
+				{/* <div className="resy-form"> */}
+				<Form
+					addReservation={this.addReservation}
+					idNumber={this.state.reservations.length + 1}
+				/>
+				{/* </div> */}
 				<div className="resy-container">
 					<Reservations reservations={this.state.reservations} />
 				</div>
---
+++ src/Form/Form.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import "./Form.css";
+
+class Form extends Component {
+	state = {
+		name: "",
+		date: "",
+		time: "",
+		number: "",
+	};
+
+	handleChange = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+
+	submitReservation = (event) => {
+		event.preventDefault();
+		const newRes = {
+			name: this.state.name,
+			date: this.state.date,
+			time: this.state.time,
+			number: this.state.number,
+		};
+		this.props.addReservation(newRes);
+	};
+
+	render() {
+		return (
+			<form className="resy-form">
+				<input
+					type="text"
+					placeholder="Name"
+					name="name"
+					value={this.state.name}
+					onChange={(event) => this.handleChange(event)}
+				/>
+
+				<input
+					type="text"
+					placeholder="Date"
+					name="date"
+					value={this.state.date}
+					onChange={(event) => this.handleChange(event)}
+				/>
+
+				<input
+					type="text"
+					placeholder="Time"
+					name="time"
+					value={this.state.time}
+					onChange={(event) => this.handleChange(event)}
+				/>
+
+				<input
+					type="text"
+					placeholder="Number of guests"
+					name="number"
+					value={this.state.number}
+					onChange={(event) => this.handleChange(event)}
+				/>
+
+				<button onClick={this.submitReservation} type="button">
+					SUBMIT
+				</button>
+			</form>
+		);
+	}
+}
+
+export default Form;
