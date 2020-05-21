9c5d0974b3303fc810b8c622a99add1111b77491
emackinnon1 <emackinnon100390@gmail.com>
2020-05-21 11:31:23 -0600

    Form test that checks to make sure upon data input the value changes is passing

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -4,17 +4,33 @@ import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/";
 
 describe("Form", () => {
-	it("should render the correct information", () => {
-		const { getByPlaceholderText, debug } = render(<Form />);
+	describe("unit tests", () => {
+		it("should render the correct information", () => {
+			const { getByPlaceholderText } = render(<Form />);
 
-		const nameInput = getByPlaceholderText("Name");
-		const dateInput = getByPlaceholderText("Date");
-		const timeInput = getByPlaceholderText("Time");
-		const numberInput = getByPlaceholderText("Number of guests");
+			const nameInput = getByPlaceholderText("Name");
+			const dateInput = getByPlaceholderText("Date");
+			const timeInput = getByPlaceholderText("Time");
+			const numberInput = getByPlaceholderText("Number of guests");
 
-		expect(nameInput).toBeInTheDocument();
-		expect(dateInput).toBeInTheDocument();
-		expect(timeInput).toBeInTheDocument();
-		expect(numberInput).toBeInTheDocument();
+			expect(nameInput).toBeInTheDocument();
+			expect(dateInput).toBeInTheDocument();
+			expect(timeInput).toBeInTheDocument();
+			expect(numberInput).toBeInTheDocument();
+		});
+
+		it("should change value when data is put into the form", () => {
+			const { getByPlaceholderText, getByDisplayValue, debug } = render(
+				<Form />
+			);
+
+			const nameInput = getByPlaceholderText("Name");
+
+			fireEvent.change(nameInput, { target: { value: "B.J. Lover" } });
+
+			const nameData = getByDisplayValue("B.J. Lover");
+
+			expect(nameData).toBeInTheDocument();
+		});
 	});
 });
